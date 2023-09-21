8978f1fd186ee4ccf67aedd2ca76843ee0f0e7c6
tafadzwabchingore <tafadzwabchingore@gmail.com>
2023-09-21 04:11:28 -0600

    week 02

---
+++ prepare/Learning02/Entry.cs
@@ -0,0 +1,9 @@
+public class Entry
+{
+    public string _date;
+    public string _promptText;
+    public string _entryText;
+
+    public void Display() 
+    {}
+}
---
+++ prepare/Learning02/Journal.cs
@@ -0,0 +1,16 @@
+public class Journal 
+{
+    public List<Entry> _entries;
+
+    public void AddEntry(Entry newEntry)
+    {}
+
+    public void DisplayAll()
+    {}
+
+    public void SaveToFile(String file)
+    {}
+
+    public void LoadFromFile(String file)
+    {}
+}
